19136fbdc1d2b3fc6e942142e15a3bffefbb9234
Andreas Rammhold <andreas@rammhold.de>
2016-12-02 15:21:23 +0100

    Enter the SERVER_HOME directory before trying to start gunicorn

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -3,5 +3,6 @@
 #if settings-file not existing, template it
 [ ! -f $CONFIG_HOME/syncserver.ini ] && dockerize -template $CONFIG_HOME/syncserver.ini.j2:$CONFIG_HOME/syncserver.ini
 
+cd $SERVER_HOME
 # run sync-server
 ./local/bin/gunicorn --paste $CONFIG_HOME/syncserver.ini
